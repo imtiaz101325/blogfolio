4e828eebe144ba1f994ab04c26ca3c26a211993b
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-08 00:29:58 +0600

    Setup blog boilerplate

---
+++ blog/README.md
@@ -0,0 +1,3 @@
+# Blog
+
+Simple static blog.
---
+++ blog/gatsby-config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  siteMetadata: {
+    title: `Interviewing Blog`,
+    author: {
+      name: `Sk Imtiaz Ahmed`,
+      summary: `Front-end guy. Experienced in shipping web apps in React. Looking for opportunities. Prefers remote work.`,
+    },
+    description: `My personal blog to document code I write while preparing for technical interviews.`,
+    siteUrl: `https://imtiaz101325.github.io/interviewing/`,
+    social: {
+      twitter: `imtiaz101325`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Interviewing Blog`,
+        short_name: `InterviewingBlog`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
