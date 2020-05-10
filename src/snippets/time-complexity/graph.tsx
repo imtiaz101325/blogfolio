bee35fe8a8397ae53a22302d06a080beb04b368b
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-11 03:34:20 +0600

    Blog post `Time Complexity` - tried generating running time data

--- src/snippets/time-complexity/graph.tsx
+++ src/snippets/time-complexity/graph.tsx
@@ -1,12 +1,51 @@
 import React, { useState } from "react";
 
+import {
+  linearSearch,
+  binarySearch,
+  generateRandomArray,
+  mapFunctionPerformanceToInputs,
+} from "./simplePerformance";
+
 function Graph() {
-  const [sliderValue, setSliderValue] = useState("0");
+  const [sliderValue, setSliderValue] = useState("10000");
+  const [inputList, setInputList] = useState([]);
 
   function handleSliderValue(event: React.FormEvent<HTMLInputElement>) {
     const target = event.target as HTMLInputElement;
 
     setSliderValue(target.value);
+  }
+
+  function generateInputList() {
+    const limit = parseInt(sliderValue, 10);
+    const list = [];
+
+    for (let i = 0; i < limit; i++) {
+      list.push(generateRandomArray(i, true));
+    }
+
+    setInputList(list);
+    generatePerformanceGraphs(list);
+  }
+
+  function generatePerformanceGraphs(list) {
+    const target = Math.floor(Math.random() * list.length);
+
+    const linearSearchData = mapFunctionPerformanceToInputs(
+      linearSearch,
+      list,
+      target
+    );
+
+    const binarySearchData = mapFunctionPerformanceToInputs(
+      binarySearch,
+      list,
+      target
+    );
+
+    console.log(linearSearchData);
+    console.log(binarySearchData);
   }
 
   return (
@@ -15,11 +54,12 @@ function Graph() {
         type="range"
         name="slider"
         id="slider"
-        min="0"
-        max="100000"
+        min="10000"
+        max="20000"
         value={sliderValue}
         onChange={handleSliderValue}
       />
+      <button onClick={generateInputList}>Generate</button>
     </div>
   );
 }
--- src/snippets/time-complexity/simplePerformance.ts
+++ src/snippets/time-complexity/simplePerformance.ts
@@ -1,4 +1,4 @@
-function linearSearch(numbers: [number], element: number) {
+function linearSearch(numbers: number[], element: number) {
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] === element) {
       return i;
@@ -9,7 +9,7 @@ function linearSearch(numbers: [number], element: number) {
   return -1;
 }
 
-function binarySearch(numbers: [number], element: number) {
+function binarySearch(numbers: number[], element: number) {
   let startIndex = 0;
   let endIndex = numbers.length - 1;
 
@@ -47,3 +47,26 @@ function generateRandomArray(len: number, sorted: boolean) {
 
   return numList;
 }
+
+function mapFunctionPerformanceToInputs(
+  fn: Function,
+  list: number[][],
+  target: number
+) {
+  return list.map(numbers => {
+    const t1 = performance.now() * 1000;
+    
+    fn(numbers, target);
+
+    const t2 = performance.now() * 1000;
+
+    return (t2 - t1);
+  });
+}
+
+export {
+  linearSearch,
+  binarySearch,
+  generateRandomArray,
+  mapFunctionPerformanceToInputs,
+};
