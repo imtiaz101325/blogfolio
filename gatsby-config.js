e25d48d8c319d25f5d997999deb085a2e506c4a0
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-10 18:01:42 +0600

    Added support for markdown code snippets

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,12 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          {
+            resolve: `gatsby-remark-embed-snippet`,
+            options: {
+              directory: `${__dirname}/snippets/`
+            },
+          },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
