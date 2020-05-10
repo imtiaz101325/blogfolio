f50f94a63c86fa5cfc2f7e2a207aa48671d8ca6a
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-11 01:32:54 +0600

    Blog post `Time Complexity` - Adde input slider

--- src/snippets/time-complexity/graph.tsx
+++ src/snippets/time-complexity/graph.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Graph() {
-  return <div>Graph</div>;
+  const [sliderValue, setSliderValue] = useState("0");
+
+  function handleSliderValue(event: React.FormEvent<HTMLInputElement>) {
+    const target = event.target as HTMLInputElement;
+
+    setSliderValue(target.value);
+  }
+
+  return (
+    <div>
+      <input
+        type="range"
+        name="slider"
+        id="slider"
+        min="0"
+        max="100000"
+        value={sliderValue}
+        onChange={handleSliderValue}
+      />
+    </div>
+  );
 }
 
 export default Graph;
