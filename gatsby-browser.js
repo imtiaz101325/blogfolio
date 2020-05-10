d316fe4bb45553bddf7f5b58b1ac914fc49c71b4
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-10 19:40:47 +0600

    Added Firacode font for ligatures

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,6 @@
 import "prismjs/themes/prism-solarizedlight.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+
+// Code snippet fonts with ligatures
+import "firacode";
+import "./src/utils/prism-override.css";
